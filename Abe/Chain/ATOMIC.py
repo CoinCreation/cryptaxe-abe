35ed35e36466d46bca2fe2aca300e42779efc4e9
cryptaxe <cryptaxe@rat>
2015-05-09 11:19:59 -0700

    Fixed address and script version for atomic

--- Abe/Chain/ATOMIC.py
+++ Abe/Chain/ATOMIC.py
@@ -4,8 +4,8 @@ class ATOMIC(Sha256Chain):
     def __init__(chain, **kwargs):
         chain.name = 'ATOMIC'
         chain.code3 = 'ATO'
-        chain.address_version = '\x00'
-        chain.script_addr_vers = '\x05'
+        chain.address_version = '\x17'
+        chain.script_addr_vers = '\x0f'
         chain.magic = '\xd8\xe6\xfe\xf5'
         Sha256Chain.__init__(chain, **kwargs)
 
